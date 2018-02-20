b19698bccc973ab9b49c758608f1eda3e72ae860
Felipe Wollmann <felipe@Genesys.local>
2018-02-20 08:05:36 -0300

    Initial commit

---
+++ BancoDeDadosFinancas/BancoDeDadosFinancas/DBContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDeDadosFinancas
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base()
+        {
+        }
+
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public virtual DbSet<Conta> Contas { get; set; }
+        public virtual DbSet<TipoMovimento> TipoMovimento { get; set; }
+        public virtual DbSet<Movimento> Movimentos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors) // <-- Coloque um Breakpoint aqui para conferir os erros de validação.
+                {
+                    Console.WriteLine("Entidade do tipo \"{0}\" no estado \"{1}\" tem os seguintes erros de validação:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Erro: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            catch (DbUpdateException e)
+            {
+                foreach (var eve in e.Entries)
+                {
+                    Console.WriteLine("Entidade do tipo \"{0}\" no estado \"{1}\" tem os seguintes erros de validação:",
+                        eve.Entity.GetType().Name, eve.State);
+                }
+                throw;
+            }
+            catch (SqlException s)
+            {
+                Console.WriteLine("- Message: \"{0}\", Data: \"{1}\"",
+                            s.Message, s.Data);
+                throw;
+            }
+        }
+    }
+}
